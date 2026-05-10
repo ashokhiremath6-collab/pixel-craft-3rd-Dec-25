79ae4acf1748b9e8f7d2f528a38f78df658be45a
ashokhiremath6-collab <ashokhiremath6@gmail.com>
2026-05-10 12:54:25 +0000

    Drop 4 orphaned tables (session, userRoles, favorite_render_styles, works_order_documents)

---
+++ migrations/0003_drop_orphaned_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS "session";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "userRoles";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "favorite_render_styles";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "works_order_documents";
